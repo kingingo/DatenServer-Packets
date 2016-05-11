3ff737093c8c683efc057683237af7704d149273
root <root@mcgalaxy.de>
2016-05-11 18:25:09 +0200

    Booster update

--- src/dev/wolveringer/dataserver/protocoll/packets/PacketVersion.java
+++ src/dev/wolveringer/dataserver/protocoll/packets/PacketVersion.java
@@ -5,7 +5,7 @@ public class PacketVersion {
 	public static final String PROTOCOLL_VERSION;
 	
 	static {
-		String newVersion = "669e94e9b6dff897aa0dcb171a24b6c1a4fa7bcd"; //Will replaced by maven
+		String newVersion = "ac719a7424283ef925a5d387dcb06547c929e60b"; //Will replaced by maven
 		if(newVersion.length() >= 9)
 			if(newVersion.substring(1, 9).equalsIgnoreCase("buildId"))
 				newVersion =  DEFAULT_PROTOCOLL_VERSION;
