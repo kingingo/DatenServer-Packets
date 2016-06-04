400e75fa56e766caad98cdd9cd3a195040e6e2fc
root <root@mcgalaxy.de>
2016-06-04 18:47:00 +0200

    Changed packet ids

--- src/dev/wolveringer/dataserver/protocoll/packets/Packet.java
+++ src/dev/wolveringer/dataserver/protocoll/packets/Packet.java
@@ -8,23 +8,25 @@ import lombok.Getter;
 
 public abstract class Packet {
 	public static final String PROTOCOLL_VERSION;
-	
+
 	static {
 		PROTOCOLL_VERSION = PacketVersion.PROTOCOLL_VERSION;
 	}
-	
+
 	public static enum PacketDirection {
-		TO_CLIENT,
-		TO_SERVER;
-		private PacketDirection() {}
+		TO_CLIENT, TO_SERVER;
+		private PacketDirection() {
+		}
 	}
-	
+
 	private static Constructor<? extends Packet> clientBoundedPackets[] = new Constructor[256];
+	private static int clientPacketIdIndex = 0;
 	private static Constructor<? extends Packet> serverBoundedPackets[] = new Constructor[256];
-	
+	private static int serverPacketIdIndex = 0;
+
 	public static boolean client = false;
-	
-	public static Packet createPacket(int id, DataBuffer buffer,PacketDirection direction) {
+
+	public static Packet createPacket(int id, DataBuffer buffer, PacketDirection direction) {
 		try {
 			Constructor<? extends Packet> c = direction == PacketDirection.TO_SERVER ? serverBoundedPackets[id] : clientBoundedPackets[id];
 			if (c != null) {
@@ -40,7 +42,7 @@ public abstract class Packet {
 		return null;
 	}
 
-	public static int getPacketId(Packet packet,PacketDirection direction) {
+	public static int getPacketId(Packet packet, PacketDirection direction) {
 		int i = 0;
 		for (Constructor<?> c : direction == PacketDirection.TO_SERVER ? serverBoundedPackets : clientBoundedPackets) {
 			if (c != null)
@@ -50,104 +52,88 @@ public abstract class Packet {
 		}
 		return -1;
 	}
-	
-	protected static void registerPacket(int id,Class<? extends Packet> packet,PacketDirection direction){
+
+	protected static void registerPacket(PacketDirection direction, Class<? extends Packet> packet) {
 		try {
-			if(direction == PacketDirection.TO_SERVER)
-				serverBoundedPackets[id] = packet.getConstructors().length == 1 ? (Constructor<? extends Packet>) packet.getConstructors()[0] : packet.getConstructor();
+			if (direction == PacketDirection.TO_SERVER)
+				serverBoundedPackets[serverPacketIdIndex++] = packet.getConstructors().length == 1 ? (Constructor<? extends Packet>) packet.getConstructors()[0] : packet.getConstructor();
 			else
-				clientBoundedPackets[id] = packet.getConstructors().length == 1 ? (Constructor<? extends Packet>) packet.getConstructors()[0] : packet.getConstructor();
+				clientBoundedPackets[clientPacketIdIndex++] = packet.getConstructors().length == 1 ? (Constructor<? extends Packet>) packet.getConstructors()[0] : packet.getConstructor();
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		} catch (NoSuchMethodException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	static {
-		registerPacket(0xFF, PacketDisconnect.class, PacketDirection.TO_CLIENT);
-		registerPacket(0xFF, PacketDisconnect.class, PacketDirection.TO_SERVER);
+		registerPacket(PacketDirection.TO_SERVER, PacketDisconnect.class);
+
+		registerPacket(PacketDirection.TO_SERVER, PacketHandshakeInStart.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInBanStatsRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInChangePlayerSettings.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInPlayerSettingsRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInServerSwitch.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInStatsEdit.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInStatsRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInGetServer.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInBanPlayer.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketServerAction.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInServerStatus.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInServerStatusRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketChatMessage.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketServerMessage.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketForward.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketSettingUpdate.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInLobbyServerRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketInTopTenRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketSkinRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketSkinSet.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketEventCondition.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketEventTypeSettings.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketLanguageRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketPlayerIdRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketReportRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketReportEdit.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketBoosterStatusRequest.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketBoosterActive.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketPing.class);
+		registerPacket(PacketDirection.TO_SERVER, PacketPong.class);
 		
+		registerPacket(PacketDirection.TO_CLIENT, PacketDisconnect.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutHandschakeAccept.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutPacketStatus.class);
 		
-		registerPacket(0x00, PacketHandshakeInStart.class, PacketDirection.TO_SERVER);
-		registerPacket(0x01, PacketInBanStatsRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x02, PacketInChangePlayerSettings.class, PacketDirection.TO_SERVER);
-		registerPacket(0x03, PacketInPlayerSettingsRequest.class, PacketDirection.TO_SERVER);
-		//registerPacket(0x04, PacketInConnectionStatus.class,  PacketDirection.TO_SERVER);
-		registerPacket(0x05, PacketInServerSwitch.class, PacketDirection.TO_SERVER);
-		registerPacket(0x06, PacketInStatsEdit.class, PacketDirection.TO_SERVER);
-		registerPacket(0x07, PacketInStatsRequest.class, PacketDirection.TO_SERVER);
-		//registerPacket(0x08, PacketInUUIDRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x09, PacketInGetServer.class, PacketDirection.TO_SERVER);
-		registerPacket(0x0A, PacketInBanPlayer.class, PacketDirection.TO_SERVER);
-		//registerPacket(0x0B, PacketInNameRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x0C, PacketServerAction.class, PacketDirection.TO_SERVER);
-		registerPacket(0x0D, PacketInServerStatus.class, PacketDirection.TO_SERVER);
-		registerPacket(0x0E, PacketInServerStatusRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x0F, PacketChatMessage.class, PacketDirection.TO_SERVER);
-		registerPacket(0x10, PacketServerMessage.class, PacketDirection.TO_SERVER);
-		registerPacket(0x11, PacketForward.class, PacketDirection.TO_SERVER);
-		registerPacket(0x12, PacketSettingUpdate.class, PacketDirection.TO_SERVER);
-		registerPacket(0x13, PacketInLobbyServerRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x14, PacketInTopTenRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x15, PacketSkinRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x16, PacketSkinSet.class, PacketDirection.TO_SERVER);
-		registerPacket(0x17, PacketEventCondition.class, PacketDirection.TO_SERVER);
-		registerPacket(0x18, PacketEventTypeSettings.class, PacketDirection.TO_SERVER);
-		registerPacket(0x19, PacketLanguageRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x1A, PacketPlayerIdRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x1B, PacketReportRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x1C, PacketReportEdit.class, PacketDirection.TO_SERVER);
-		registerPacket(0x1D, PacketBoosterStatusRequest.class, PacketDirection.TO_SERVER);
-		registerPacket(0x1E, PacketBoosterActive.class, PacketDirection.TO_SERVER);
-		
-		registerPacket(0xF0, PacketOutPacketStatus.class, PacketDirection.TO_CLIENT);
-		
-		registerPacket(0x00, PacketOutHandschakeAccept.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x01, PacketOutStats.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x02, PacketOutPlayerSettings.class, PacketDirection.TO_CLIENT);
-		//registerPacket(0x03, PacketOutUUIDResponse.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x04, PacketOutPlayerServer.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x05, PacketOutBanStats.class, PacketDirection.TO_CLIENT);
-		//registerPacket(0x06, PacketOutNameResponse.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x07, PacketServerAction.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x08, PacketOutServerStatus.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x09, PacketChatMessage.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x0A, PacketOutGammodeChange.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x0B, PacketServerMessage.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x0C, PacketForward.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x0D, PacketSettingUpdate.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x0E, PacketOutLobbyServer.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x0F, PacketOutTopTen.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x10, PacketSkinData.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x11, PacketEventFire.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x12, PacketLanguageResponse.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x13, PacketPlayerIdResponse.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x14, PacketReportResponse.class, PacketDirection.TO_CLIENT);
-		registerPacket(0x15, PacketBoosterStatusResponse.class, PacketDirection.TO_CLIENT);
-		
-		registerPacket(0xFF, PacketDisconnect.class, PacketDirection.TO_CLIENT);
-		registerPacket(0xFF, PacketDisconnect.class, PacketDirection.TO_SERVER);
-		
-		registerPacket(0xFE, PacketPing.class, PacketDirection.TO_CLIENT);
-		registerPacket(0xFE, PacketPing.class, PacketDirection.TO_SERVER);
-		registerPacket(0xFD, PacketPong.class, PacketDirection.TO_CLIENT);
-		registerPacket(0xFD, PacketPong.class, PacketDirection.TO_SERVER);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutStats.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutPlayerSettings.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutPlayerServer.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutBanStats.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketServerAction.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutServerStatus.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketChatMessage.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutGammodeChange.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketServerMessage.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketForward.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketSettingUpdate.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutLobbyServer.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketOutTopTen.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketSkinData.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketEventFire.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketLanguageResponse.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketPlayerIdResponse.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketReportResponse.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketBoosterStatusResponse.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketPing.class);
+		registerPacket(PacketDirection.TO_CLIENT, PacketPong.class);
 	}
 
 	@Getter
 	private UUID packetUUID = UUID.randomUUID();
-	
+
 	public Packet() {
 	}
-	
-	public void read(DataBuffer buffer){
-		throw new NullPointerException("Packet is write only");
-	}
-	public void write(DataBuffer buffer){
-		throw new NullPointerException("Packet is read only");
-	}
-	//TODO
-	//Messages
-	//Bann
+
+	public abstract void read(DataBuffer buffer);
+
+	public abstract void write(DataBuffer buffer);
 }
