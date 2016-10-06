944973d5fa963840dc072221f0cdd65ef5a9c107
Janmm14 <computerjanimaus@yahoo.de>
2016-10-06 22:24:29 +0200

    Readd deleted class and put it in the right package

---
+++ src/eu/epicpvp/dataserver/protocoll/packets/PacketVersion.java
@@ -0,0 +1,14 @@
+package eu.epicpvp.dataserver.protocoll.packets;
+
+public class PacketVersion {
+	private static final String DEFAULT_PROTOCOLL_VERSION = "undefined";
+	public static final String PROTOCOLL_VERSION;
+	
+	static {
+		String newVersion = "e07fbb139590441cd7a0441b149d8fd8f01aee50"; //Will replaced by maven //not anymore
+		if(newVersion.length() >= 9)
+			if(newVersion.substring(1, 9).equalsIgnoreCase("buildId"))
+				newVersion =  DEFAULT_PROTOCOLL_VERSION;
+		PROTOCOLL_VERSION = newVersion;
+	}
+}
